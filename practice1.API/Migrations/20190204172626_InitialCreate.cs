f11e0d4d1678331e59de56bab7e1ee7abf2a273c
Rehan <rehan@Rehans-MacBook-Pro.local>
2019-02-05 22:01:39 +0500

    Initial Commit

---
+++ practice1.API/Migrations/20190204172626_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace practice1.API.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Persons",
+                columns: table => new
+                {
+                    personId = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:AutoIncrement", true),
+                    personName = table.Column<string>(nullable: true),
+                    perosnAge = table.Column<int>(nullable: false),
+                    personAddress = table.Column<string>(nullable: true),
+                    gender = table.Column<string>(nullable: true),
+                    picUrl = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persons", x => x.personId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Persons");
+        }
+    }
+}
---
+++ practice1.API/Model/Person.cs
@@ -0,0 +1,16 @@
+namespace practice1.API.Model
+{
+    public class Person
+    {
+        public int personId { get; set; }
+        public string personName { get; set; }
+
+        public int personAge { get; set; }  
+
+        public string personAddress { get; set; }
+
+        public string gender {get;set;}
+
+        public string picUrl { get; set; }
+    }
+}
